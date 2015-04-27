f8226ef7a8c132fed0a9ba22580469cf3eb6e65f
andresmit <andresmt@ut.ee>
2015-04-27 14:59:33 +0300

    'Dept Equity Ratio'

--- DERatio.py
+++ DERatio.py
@@ -8,13 +8,23 @@ path = "G:/intraQuarter"
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path+'/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
-    df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio','Price','SP500'])
+    df = pd.DataFrame(columns = ['Date',
+                                 'Unix',
+                                 'Ticker',
+                                 'DE Ratio',
+                                 'Price',
+                                 'stock_p_change',
+                                 'sp500_p_change',
+                                 'SP500'])
 
     sp500_df = pd.DataFrame.from_csv("YAHOO-INDEX_GSPC.csv")
-
+    ticker_list = []
     for each_dir in stock_list[1:25]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("\\")[1]
+        ticker_list.append(ticker)
+        starting_stock_value = False
+        starting_sp500_value = False
         if len(each_file) > 0:
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
@@ -33,18 +43,28 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                         row = sp500_df[(sp500_df.index == sp500_date)]
                         sp500_value = float(row["Adjusted Close"])
 
+                    if not starting_stock_value:
+                        starting_stock_value = stock_price
 
+                    if not starting_sp500_value:
+                        starting_sp500_value = sp500_value
                     stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+                    stock_p_change = ((stock_price - starting_stock_value) / starting_stock_value) * 100
+                    sp500_p_change = ((sp500_value - starting_sp500_value) / starting_sp500_value) * 100
+
                     #print("stock_price:",stock_price,"ticker:", ticker)
                         
                     
+
 
                     df = df.append({'Date':date_stamp,
                                     'Unix':unix_time,
                                     'Ticker':ticker,
                                     'DE Ratio':value,
                                     'Price':stock_price,
-                                    'SP500':sp500_value}, ignore_index = True)
+                                    'stock_p_change':stock_p_change,
+                                    'SP500':sp500_value,
+                                    'sp500_p_change':sp500_p_change}, ignore_index = True)
                 except Exception as e:
                     pass
 
